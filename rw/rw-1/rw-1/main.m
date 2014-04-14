e26bedad44af051ea40c9011689e256b48559187
Tomasz Giereś <tomasz.gieres@lunarlogic.io>
2014-04-14 15:04:56 +0200

    Initial Commit

---
+++ rw/rw-1/rw-1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rw-1
+//
+//  Created by Tomasz Giereś on 14/04/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
