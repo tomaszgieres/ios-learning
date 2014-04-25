091a09e6e3d6b8d1757a49fd9d604b85771da211
Tomasz Giereś <tomasz.gieres@lunarlogic.io>
2014-04-25 13:20:46 +0200

    Initial Commit

---
+++ tbnr/tbnr_book_2/tbnr_book_2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tbnr_book_2
+//
+//  Created by Tomasz Giereś on 25/04/14.
+//  Copyright (c) 2014 Gieres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
